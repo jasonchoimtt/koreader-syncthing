28563a107f3a0fd7cec15794845e0e5576965ba1
Jason Choi <jasonchoi.mtt@gmail.com>
2023-11-04 19:57:17 +0800

    feat: move syncthing item to file manager tools menu

--- syncthing.koplugin/insert_menu.lua
+++ syncthing.koplugin/insert_menu.lua
@@ -1,2 +1,12 @@
--- Add Syncthing to the More Tools menu
-require("frontend/ui/plugin/insert_menu").add("syncthing")
+-- Add Syncthing to the Tools menu in File Manager, after cloud storage item
+local filemanager_order = require("ui/elements/filemanager_menu_order")
+
+local pos = 1
+for index, value in ipairs(filemanager_order.tools) do
+    if value == "cloud_storage" then
+        pos = index + 1
+        break
+    end
+end
+
+table.insert(filemanager_order.tools, pos, "syncthing")
